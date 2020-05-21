7a29f876543c48f998a770669a8b4d7cc741427f
kevinos1998 <64386262+kevinos1998@users.noreply.github.com>
2020-05-21 21:28:33 +0100

    Delete __init__.py

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-# import routes file containing page view functions
-from app import routes
-app.config['SECRET_KEY'] = 'any secret string'
-app.jinja_env.cache = {}
